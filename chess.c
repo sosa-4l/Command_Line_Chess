460b092eb9fe7aaf04f571ec9e12497fc7cd5e4c
Belo-Osagie Eseosa <eseosadaboy@gmail.com>
2025-01-17 13:37:58 -0500

    implemented move verification and player move acceptance

--- chess.c
+++ chess.c
@@ -134,6 +134,274 @@ struct peice *black_arr[2][8] = {NULL};
     printf(" --------------------------------\n");
 }
 
+bool pawn_move(int old_row, int old_col, int new_row, int new_col){
+    int row_dif = abs(old_row-new_row);
+    if (row_dif > 2 || row_dif == 0){
+        return false;
+    } else if (row_dif == 2) {
+        if (board[old_row][old_col]->moves != 0) {
+            return false;
+        }
+        if (old_col != new_col) return false;
+        if (board[new_row][new_col]) return false;
+        return true;
+    } else {
+        if (old_col == new_col && !board[new_row][new_col]) {
+            return true;
+        } else if (abs(old_col - new_col) == 1 && board[new_row][new_col]->team != board[old_row][old_col]->team){
+            return true;
+        } else{
+            return false;
+        }
+    }
+}
+
+bool bishop_move(int old_row, int old_col, int new_row, int new_col) {
+    int row_increment, col_increment;
+    row_increment = (old_row < new_row) ? 1 : -1;
+    col_increment = (old_col < new_col) ? 1 : -1;
+
+    int cur_row = old_row + row_increment;
+    int cur_col = old_col + col_increment;
+    while(cur_col>-1 && cur_col<8 && cur_row>-1 && cur_row<8) {
+        if(cur_row== new_row && cur_col== new_col){
+            if(!board[new_row][new_col]) {
+                return true;
+            } else return !(board[new_row][new_col]->team == board[old_row][old_col]->team);
+        } else {
+            if (board[cur_row][cur_col]) {
+                return false;
+            }
+        }
+        cur_col = cur_col + col_increment;
+        cur_row = cur_row + row_increment;
+    }
+    return false;
+}
+
+bool knight_move(int old_row, int old_col, int new_row, int new_col) {
+    int row_diff = new_row - old_row;
+    int col_diff = new_col - old_col;
+
+    bool check_1 = (abs(row_diff) == 1) && (abs(col_diff) == 2);
+    bool check_2 = (abs(row_diff) == 2) && (abs(col_diff) == 1);
+
+    if(!(check_1 || check_2)) return false;
+
+    if(!board[new_row][new_col]) {
+        return true;
+    } else return !(board[new_row][new_col]->team == board[old_row][old_col]->team);
+}
+
+bool rook_move(int old_row, int old_col, int new_row, int new_col) {
+    int count;
+    if (old_row == new_row) {
+        count = (old_col < new_col) ? 1 : -1;
+        int cur_col = old_col+count;
+        while(cur_col >-1 && cur_col<8) {
+        if(cur_col== new_col){
+            if(!board[new_row][new_col]) {
+                return true;
+            } else return !(board[new_row][new_col]->team == board[old_row][old_col]->team);
+        } else {
+            if (board[old_row][cur_col]) {
+                return false;
+            }
+        }
+        cur_col += count;
+        }
+    } else if (old_col == new_col) {
+        count = (old_row < new_row) ? 1 : -1;
+        int cur_row = old_row+count;
+        while(cur_row >-1 && cur_row<8) {
+        if(cur_row== new_row){
+            if(!board[new_row][new_col]) {
+                return true;
+            } else return !(board[new_row][new_col]->team == board[old_row][old_col]->team);
+        } else {
+            if (board[cur_row][old_col]) {
+                return false;
+            }
+        }
+        cur_row += count;
+        }
+    }
+    return false;
+}
+
+bool queen_move (int old_row, int old_col, int new_row, int new_col) {
+    return (rook_move(old_row, old_col, new_row, new_col) || bishop_move(old_row, old_col, new_row, new_col));
+}
+
+bool king_move(int old_row, int old_col, int new_row, int new_col) {
+    int row_diff = new_row - old_row;
+    int col_diff = new_col - old_col;
+
+    bool check_1 = (abs(row_diff) == 1) || (abs(row_diff) == 0);
+    bool check_2 = (abs(col_diff) == 1) || (abs(col_diff) == 0);
+
+    if(!(check_1 && check_2)) return false;
+
+    if(!board[new_row][new_col]) {
+        return true;
+    } else return !(board[new_row][new_col]->team == board[old_row][old_col]->team);
+}
+
+
+
+void black_player();
+
+void white_player(){ 
+    if(white_check(white_arr[1][3]->row_num, white_arr[1][3]->col_num )) {
+       printf("Player White is on check\n");
+       if(white_checkmate()) {
+        printf("And that is checkmate..\nPlayer Black Wins!!\n");
+        return;
+       }
+   }
+   
+    printf("White Player's Turn...\n");
+    int init_col;
+    int col;
+    int old_row;
+    int new_row;
+    if (scanf(" %c%d %c%d", &init_col, &old_row, &col, &new_row) != 4) {
+        printf("Invalid move entry, retry\n");
+        clear_input();
+        white_player();
+        return;
+    }
+    init_col = toupper(init_col);
+    col = toupper(col);
+
+    int old_col = abs(init_col-72);
+    int new_col = abs(col-72);
+    old_row = old_row - 1;
+    new_row = new_row -1;
+
+    struct peice *old_peice = NULL;
+
+   if (old_row > 7 || old_row < 0 || new_row > 7 || new_row < 0) {
+        printf("Invalid move, Try Again\n");
+        white_player();
+        return;
+    } 
+    if (old_col > 7 || old_col < 0 || new_col > 7 || new_col < 0) {
+        printf("Invalid move, Try Again\n");
+        white_player();
+        return;
+    } 
+    if (board[old_row][old_col] == NULL || board[old_row][old_col]->team == 'B' ) {
+        printf("Invalid move, Try Again\n");
+        white_player();
+        return;
+    } else {
+        if(board[old_row][old_col]->name == 'P' && pawn_move(old_row, old_col, new_row, new_col)) {
+            old_peice = update_board(old_row, old_col, new_row, new_col);
+        } else if (board[old_row][old_col]->name == 'B' && bishop_move(old_row, old_col, new_row, new_col)) {
+            old_peice = update_board(old_row, old_col, new_row, new_col);
+        } else if (board[old_row][old_col]->name == 'R' && rook_move(old_row, old_col, new_row, new_col)) {
+            old_peice = update_board(old_row, old_col, new_row, new_col);
+        } else if (board[old_row][old_col]->name == 'Q' && queen_move(old_row, old_col, new_row, new_col)) {
+            old_peice = update_board(old_row, old_col, new_row, new_col);
+        } else if (board[old_row][old_col]->name == 'N' && knight_move(old_row, old_col, new_row, new_col)) {
+            old_peice = update_board(old_row, old_col, new_row, new_col);
+        } else if (board[old_row][old_col]->name == 'K' && king_move(old_row, old_col, new_row, new_col)) {
+            old_peice = update_board(old_row, old_col, new_row, new_col);
+        } else {
+            printf("Invalid move, Try again.\n");
+            white_player();
+            return;
+        }
+    }
+
+    if(white_check(white_arr[1][3]->row_num, white_arr[1][3]->col_num )) {
+        printf("Invalid Move, You are on Check\n");
+        revert_board(old_peice, old_row, old_col, new_row, new_col);
+        white_player();
+        return;
+    }
+
+    print_board();
+    black_player();
+}
+
+void black_player(){
+    if(black_check(black_arr[1][3]->row_num, black_arr[1][3]->col_num )) {
+        printf("Player Black is on check\n");
+        if(black_checkmate()) {
+        printf("And that is checkmate..\nPlayer White Wins!!\n");
+        return;
+        }
+   }
+   
+    printf("Black Player's Turn...\n");
+    int init_col;
+    int col;
+    int old_row;
+    int new_row;
+    int scan = scanf(" %c%d %c%d", &init_col, &old_row, &col, &new_row);
+    if (scan != 4) {
+        printf("Invalid move entry, retry\n");
+        clear_input();
+        black_player();
+        return;
+    }
+    init_col = toupper(init_col);
+    col = toupper(col);
+
+    int old_col = abs(init_col-72);
+    int new_col = abs(col-72);
+    old_row = old_row - 1;
+    new_row = new_row -1;
+
+    struct peice *old_peice = NULL;
+
+   if (old_row > 7 || old_row < 0 || new_row > 7 || new_row < 0) {
+        printf("Invalid move, Try Again\n");
+        black_player();
+        return;
+    } 
+    if (old_col > 7 || old_col < 0 || new_col > 7 || new_col < 0) {
+        printf("Invalid move, Try Again\n");
+        black_player();
+        return;
+    } 
+    if (board[old_row][old_col] == NULL || board[old_row][old_col]->team == 'W' ) {
+        printf("Invalid move, Try Again\n");
+        black_player();
+        return;
+    } else {
+        if(board[old_row][old_col]->name == 'p' && pawn_move(old_row, old_col, new_row, new_col)) {
+            old_peice = update_board(old_row, old_col, new_row, new_col);
+        } else if (board[old_row][old_col]->name == 'b' && bishop_move(old_row, old_col, new_row, new_col)) {
+            old_peice = update_board(old_row, old_col, new_row, new_col);
+        } else if (board[old_row][old_col]->name == 'r' && rook_move(old_row, old_col, new_row, new_col)) {
+            old_peice = update_board(old_row, old_col, new_row, new_col);
+        } else if (board[old_row][old_col]->name == 'q' && queen_move(old_row, old_col, new_row, new_col)) {
+            old_peice = update_board(old_row, old_col, new_row, new_col);
+        } else if (board[old_row][old_col]->name == 'n' && knight_move(old_row, old_col, new_row, new_col)) {
+            old_peice = update_board(old_row, old_col, new_row, new_col);
+        } else if (board[old_row][old_col]->name == 'k' && king_move(old_row, old_col, new_row, new_col)) {
+            old_peice = update_board(old_row, old_col, new_row, new_col);
+        } else {
+            printf("Invalid move, Try again.\n");
+            black_player();
+            return;
+        }
+    }
+
+    if(black_check(black_arr[1][3]->row_num, black_arr[1][3]->col_num )) {
+        printf("Invalid Move, You are on Check\n");
+        revert_board(old_peice, old_row, old_col, new_row, new_col);
+        black_player();
+        return;
+    }
+
+    print_board();
+    white_player();
+}
+
 
  int main(){
     setlocale(LC_ALL, "");
